b03e495a13ee6d02583805767d0e54edcc4b5ade
Diego Fernandes <diego.schell.f@gmail.com>
2018-02-19 19:33:13 -0300

    #21 - Utilizando Axios

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-var ajax = function(method, url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-  
-    xhr.open(method, url);
-    xhr.send(null);
-  
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === xhr.DONE) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject('Erro na API');
-        }
-      }
-    }
-  });
-}
-
-ajax('GET', 'https://api.gith3ub.com/users/diego3g')
+axios.get('https://api.github.com/users/diego3g')
   .then(function(response) {
     console.log(response);
   })
